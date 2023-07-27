6cc5d1684a758df02fac5d0554fedc3338f3cf6c
Abu-Jayed <aj.webdevlopment@gmail.com>
2023-07-27 09:05:31 +0600

    password generator

---
+++ Password_Generator.js
@@ -0,0 +1,16 @@
+function generateRandomPassword(length) {
+  const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}|;:,.<>?";
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomIndex);
+  }
+
+  return password;
+}
+
+// Test the function
+const passwordLength = 6; // Change this value to set the desired password length
+const generatedPassword = generateRandomPassword(passwordLength);
+console.log(generatedPassword);
