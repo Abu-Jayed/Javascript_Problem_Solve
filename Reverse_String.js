0ac5a911d1a974bb21d67a5e672a950e1a04d03f
Abu-Jayed <aj.webdevlopment@gmail.com>
2023-07-27 08:33:52 +0600

    array number sum

--- Reverse_String.js
+++ Reverse_String.js
@@ -1,1 +1,12 @@
-console.log('reverse');
+function reverseString(inputString) {
+  let reversedString = "";
+  for (let i = inputString.length - 1; i >= 0; i--) {
+    reversedString += inputString.charAt(i);
+  }
+  return reversedString;
+}
+
+// Test the function
+const inputStr = "He";
+const outputStr = reverseString(inputStr);
+console.log(outputStr); // Output: "dlrow olleh"
